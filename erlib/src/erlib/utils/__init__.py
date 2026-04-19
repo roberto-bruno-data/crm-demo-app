a5e9dc9aa5fd2aecec7955604a50c6e9c6357557
roberto-bruno-data <66418061+roberto-bruno-data@users.noreply.github.com>
2026-04-19 16:26:16 +0200

    Update __init__.py

--- erlib/src/erlib/utils/__init__.py
+++ erlib/src/erlib/utils/__init__.py
@@ -1,4 +1,4 @@
 from erlib.utils.split_container import Split
 from erlib.utils.constants import ATTRIBUTES
 
-__all__ = ["normalize_shap_output", "Split", "ATTRIBUTES"]
+__all__ = ["Split", "ATTRIBUTES"]
